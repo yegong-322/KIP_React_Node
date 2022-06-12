9b0849c5bab119d9ed1f31d22d9fb0b69854db30
yegong322 <yean990830@gmail.com>
2022-06-12 20:15:00 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className='gray-background'>
+      <img src={logo} lat='logo'></img>
+      <h2>Let's develop KIP System!</h2>
+    </div>
+  );
+}
+
+export default App;
